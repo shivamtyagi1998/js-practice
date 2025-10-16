dc23ed9f7b4737682a23b3acb5616cf99547fde5
Shivam Bhardwaj <55041976+shivamtyagi1998@users.noreply.github.com>
2025-10-16 09:56:00 +0000

    explored arithmatic , unary and post and prefix operators

---
+++ 01_basics/operators.js
@@ -0,0 +1,28 @@
+
+//**************** Postfix and Prefix Incrementation ******************
+let counter = 1
+
+let a = ++counter
+console.log(`counter: ${counter} a : ${a}`) // Output :- counter: 2 a : 2 (it will increment the value by 1 before using the variable) 
+
+let b = counter++
+console.log(`counter: ${counter} b : ${b}`) // Output :- counter: 2 b : 1 (it will increment the value by 1 after using the variable)
+
+
+
+//**************** Arithmatic ******************
+
+console.log(`2^4 = ${2**4}`) // ** operator is used when we want to calculate power of any value ie 2^6, 3^4 etc...
+console.log(`2+4 = ${2+4}`) // Addition
+console.log(`2-4 = ${2-4}`) // Substraction
+console.log(`2*4 = ${2*4}`) // Multiplication
+console.log(`2/4 = ${2/4}`) // Division
+console.log(`5%4 = ${5%4}`) // Remainder
+
+
+let valOne = 5
+console.log(`-valOne = ${-valOne}`) // Output : -valOne = -5 // Unary Negative - Used to change  positive valye to negative and negative value to positive
+
+
+let valTwo = -4
+console.log(`-valTwo = ${-valTwo}`) // Output : -valOne = 4
