5dd9bac512a1418653ef49f61ef4931de5bcd845
Shivam Bhardwaj <55041976+shivamtyagi1998@users.noreply.github.com>
2025-10-16 06:46:20 +0000

    Explored javascript primitive datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,20 @@
+
+let age = 28 // number :- it is used to store number values both floating point and integer ranges from -2^53 t0 2^52-1
+
+let name = "Shivam Bhardwaj" //string :- used to store textual date 
+
+let isWorking = true //boolean :- used to store logical values either true or false.
+
+let bigNumber = 28777490809584n // bigint :- it is used to store large number values only integer.
+
+let x // undefined :- is it used to declare a variable but the value and type is not defined yet.
+console.log(x)
+
+let y = null // null :- it is used to declare a variable but shows absense of the value but we can add any value of any type later..
+console.log(y)
+y = "shivam"
+console.log(y)
+
+let idSymbol = Symbol("id") //symbol :- Used to create unique identifiers
+
+console.log(idSymbol) 
